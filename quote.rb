33af586dc19ccd71bc65b6c56a712c654c0790c3
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2024-01-17 15:58:55 -0600

    Create quote.rb

---
+++ quote.rb
@@ -0,0 +1,14 @@
+# quote.rb
+
+class Quote
+  attr_reader :text, :author
+
+  def initialize(text, author)
+    @text = text
+    @author = author
+  end
+
+  def to_s
+    "#{text} - #{author}"
+  end
+end
